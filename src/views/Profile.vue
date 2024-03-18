cf1ef6d8133e16ae8346acd85683701c04761032
teguh badrusalam <teguhbaghoy6@gmail.com>
2024-03-18 21:25:36 +0700

    add dot to see update

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -104,7 +104,7 @@ onMounted(() => {
           </div>
 
           <div class="py-12">
-            <button @click="updateUserProfile" class="group disabled:bg-gray-400 relative flex w-full justify-center rounded-full border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Update Profile</button>
+            <button @click="updateUserProfile" class="group disabled:bg-gray-400 relative flex w-full justify-center rounded-full border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Update Profile.</button>
           </div>
         </div>
       </div>
